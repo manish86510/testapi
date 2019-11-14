e88bc89d8fc54f93c498ad60e1f64baa5e43fe01
SG_Ishu <ishu.k@skysoft.net.in>
2019-11-14 18:54:32 +0530

    fixed requirements

--- Energe/urls.py
+++ Energe/urls.py
@@ -32,6 +32,7 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+
 urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),

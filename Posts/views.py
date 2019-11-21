f227921d9e0ec11580efab74580ecd7c92ce947a
Suman Kumar <suman.k@skysoft.net.in>
2019-11-21 19:02:24 +0530

    +1

--- Posts/views.py
+++ Posts/views.py
@@ -2,7 +2,9 @@ from rest_framework import viewsets, permissions
 from .models import *
 # from .serializers import *
 from .serializers.post import PostSerializer, PostCreateSerializer
-from .swagger import PostSwaggerDoc
+from .serializers.post_media import PostMediaSerializer
+from .swagger.post import PostSwaggerDoc
+from .swagger.post_media import PostMediaSwagger
 from django.utils.decorators import method_decorator
 
 
@@ -27,3 +29,22 @@ class PostViewSet(viewsets.ModelViewSet):
             return self.serializer_action_classes[self.action]
         except(KeyError, AttributeError):
             return super().get_serializer_class()
+
+
+@method_decorator(name='create', decorator=PostMediaSwagger.create())
+@method_decorator(name='list', decorator=PostMediaSwagger.list())
+@method_decorator(name='destroy', decorator=PostMediaSwagger.delete())
+@method_decorator(name='update', decorator=PostMediaSwagger.update())
+@method_decorator(name='retrieve', decorator=PostMediaSwagger.retrieve())
+class PostMediaViewSet(viewsets.ModelViewSet):
+    serializer_class = PostMediaSerializer
+    # http_method_names = ['post', 'put', 'delete']
+
+    def get_queryset(self):
+        queryset = PostMedia.objects.all()
+        return queryset
+
+    # @action(methods=['get'], url_path='/<int:post_id>')
+    def list(self, request, *args, **kwargs):
+        return super(PostMediaViewSet, self).list(request, *args, **kwargs)
+

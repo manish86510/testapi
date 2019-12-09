2c72463a52104c27bc717799233f67f289dbef22
sg_shivendra <shivendra.s@skysoft.net.in>
2019-12-09 13:25:19 +0530

    2

--- Energe/settings/base.py
+++ Energe/settings/base.py
@@ -43,7 +43,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 15
+    'PAGE_SIZE': 5
 }
 
 SIMPLE_JWT = {
